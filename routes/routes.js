face14fc68c3aa514ac969c7aaf41e43c1eecce6
osmergit <osmer1976@gmail.com>
2022-12-06 07:35:59 -0500

    JWT login

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { regusuario, showuser, shuserone, deluser, upuser  } from '../controllers/userscontroller.js';
 import { regevento, shevento, shonevent, delevento, upevento } from '../controllers/eventcontrollers.js';
-//import {login, loginjwt} from '../controllers/logincontrollers.js';
-//import {requireToken} from '../Midlewares/auth.js'
+import {login, loginjwt} from '../controllers/logincontrollers.js';
+import {requireToken} from '../Midlewares/auth.js'
 import {db} from '../mongodb.js';
 
 
@@ -12,10 +12,10 @@ export const router = express.Router();
 
 //rutas collection usuarios
 //router.post('/login1',requireToken, loginjwt );
-//router.post('/login', login );
+router.post('/login', login );
 router.post('/reguser', regusuario);
-//router.get('/shuser',requireToken, showuser);
-router.get('/shuser', showuser);
+router.get('/shuser',requireToken, showuser);
+//router.get('/shuser', showuser);
 router.get('/shuser/:id', shuserone);
 router.delete('/shuser/:id',deluser);
 router.put('/shuser/:id', upuser);
